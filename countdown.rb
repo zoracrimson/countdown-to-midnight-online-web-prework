688d0a26081f2cd4724e0c0343e37b1a9cfcb368
Kat Maldon <katharinamaldon@gmail.com>
2018-11-27 18:51:12 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,9 @@
 #write your code here
 
 def countdown(number)
-  number == 10 
+  number = 10 
   while number > 0
     puts "#{number} SECONDS!"
     number -= 1
-  if number == 0 
-    puts "HAPPY NEW YEAR!"
   end
 end
