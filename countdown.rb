42efb3d86b9b93e1615b00675d267c2898519ec7
Kat Maldon <katharinamaldon@gmail.com>
2018-11-28 16:45:48 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  number = 10 
+  while number > 0
+    number =- 1 
+    puts "#{number} SECONDS!"
+  end
+  puts "HAPPY NEW YEAR!"
 end
